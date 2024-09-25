29a9b226831ef04df2fa927c20d5c1e816588ab4
yoavg <ymgeva@gmail.com>
2024-09-25 22:07:46 +0300

    Fix a crash when using custom skill that does not have the hard coded knowledge skills

--- src/scripts/hud-builder.js
+++ src/scripts/hud-builder.js
@@ -392,6 +392,7 @@ Hooks.once('tokenActionHudCoreApiReady', async (coreModule) => {
                 }
 
                 const knowledges = this.#knowledgeSkillIds
+                    .filter((id) => actorSkills[id])
                     .filter((id) => !Settings.hideUntrainedSkills || !actorSkills[id].rt || !!actorSkills[id].rank)
                     .map((id) => ({
                         id: `categorized-${id}`,
